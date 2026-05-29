c5c69a82332107ee382d03152510920bab046289
Aneonfas <i1@live.ru>
2026-05-29 11:40:19 +0300

    Bump stylesheet after mobile nav wrap

--- worker.js
+++ worker.js
@@ -6,7 +6,7 @@ const ZIP_URL = "https://github.com/Aneonfas/nfg-amatic-player/releases/download
 const REPO_URL = "https://github.com/Aneonfas/nfg-amatic-player";
 const PACKAGE_REPO_URL = "https://github.com/Aneonfas/nfg-amatic-packages";
 const STATIC_BASE = "https://raw.githubusercontent.com/Aneonfas/nfg-amatic-player/main";
-const STATIC_REV = "2026-05-29-mobile-grid-min";
+const STATIC_REV = "2026-05-29-mobile-nav-wrap";
 
 const STATIC_ROUTES = {
   "/": "index.html",
@@ -23,6 +23,7 @@ const STATIC_ROUTES = {
   "/assets/site.css": "assets/site.css",
   "/assets/site-20260529.css": "assets/site.css",
   "/assets/site-20260529b.css": "assets/site.css",
+  "/assets/site-20260529c.css": "assets/site.css",
   "/assets/site.js": "assets/site.js",
   "/robots.txt": "robots.txt",
   "/sitemap.xml": "sitemap.xml",
